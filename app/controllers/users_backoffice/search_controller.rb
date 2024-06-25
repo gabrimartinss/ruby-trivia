a20e12f0cf0717dc5bc57893fe0ad5a2d43f11d4
gabrimartinss <gabriel.martinsdesouza2@gmail.com>
2024-06-25 20:07:17 -0300

    refactor: search questions

--- app/controllers/users_backoffice/search_controller.rb
+++ app/controllers/users_backoffice/search_controller.rb
@@ -1,5 +1,8 @@
 class UsersBackoffice::SearchController < UsersBackofficeController
   def questions
-    @questions = Question.includes(:answers).page(params[:page])
+    @questions = Question.includes(:answers)
+                         .where("lower(description) LIKE ?",
+                         "%#{params[:term].downcase}%")
+                         .page(params[:page])
   end
 end
