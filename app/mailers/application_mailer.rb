d5f765fc1dc97e14ab364aa966d9c9d17a1e9256
gabrimartinss <gabriel.martinsdesouza2@gmail.com>
2024-11-06 15:43:21 -0300

    fix

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
-class ApplicationMailer < ApplicationMailer
+class ApplicationMailer < ActionMailer::Base
   def welcome_email(user)
     @user = user
     mail(to: @user.email, subject: 'Bem-vindo ao RubyTrivia')
